1d7c3bef4c6cc0b93cb49b1af9719fdd5191fc52
ad276 <85683804+ad276@users.noreply.github.com>
2021-08-05 17:44:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,80 +1,49 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-
-var engine, world;
-var canvas;
-var palyer, playerBase;
-var computer, computerBase;
-
-var arrow;
-
-
-function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-
-  engine = Engine.create();
-  world = engine.world;
-
-  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
-  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
-  playerArcher = new PlayerArcher(
-    340,
-    playerBase.body.position.y - 180,
-    120,
-    120
-  );
-
-  computerBase = new ComputerBase(
-    width - 300,
-    random(450, height - 300),
-    180,
-    150
-  );
-  computer = new Computer(
-    width - 280,
-    computerBase.body.position.y - 153,
-    50,
-    180
-  );
-  computerArcher = new ComputerArcher(
-    width - 340,
-    computerBase.body.position.y - 180,
-    120,
-    120
-  );
-  
-  arrow = new PlayerArrow(playerArcher.body.position.x, playerArcher.body.position.y, 100, 10);
-  
-}
 
 function draw() {
-  background(180);
-
-  Engine.update(engine);
-
-  // Title
-  fill("#FFFF");
-  textAlign("center");
-  textSize(40);
-  text("EPIC ARCHERY", width / 2, 100);
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
 
  
-  playerBase.display();
-  player.display();
-  
 
-  computerBase.display();
-  computer.display();
+  Engine.update(engine);
+  ground.display();
+
+ 
+
+  for (var i = 0; i < balls.length; i++) {
+    showCannonBalls(balls[i], i);
+  }
+
+  cannon.display();
+  tower.display();
+
   
-  playerArcher.display();
-  computerArcher.display()
-  arrow.display()
-  if(keyCode === 32){
-   arrow.shoot(playerArcher.body.angle);
+}
+
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    var cannonBall = new CannonBall(cannon.x, cannon.y);
+    balls.push(cannonBall);
   }
 }
+
+//function to show the ball
+function showCannonBalls(ball, index) {
+  ball.display();
+  if (ball.body.position.x >= width || ball.body.position.y >= height - 50) {
+    Matter.World.remove(world, ball.body);
+    balls.splice(index, 1);
+  }
+}
+
+
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) { 
+    balls[balls.length - 1].shoot();
+  }
+}
+
 
 
 
